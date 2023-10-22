48835a6595769d0faa835640e4858ae830980aa9
deepak singh <djangoboy.18feb@gmail.com>
2023-10-22 21:48:44 +0530

    for lc and rc both the connection is able to open next is take the streams from each other should be very tough from here. looks really good

--- fe/src/screen/video_components/VideoWindow.js
+++ fe/src/screen/video_components/VideoWindow.js
@@ -189,40 +189,13 @@ export default function VideoWindow({ with_email,with_userid }) {
       setStream(stream);
       lc.addStream(stream);
       lc.onaddstream = (event) => {
-        
         myRef.current = {"srcObject":event.stream};
         // remoteVideoRef.current.srcObject = event.stream;
         setStream(event.stream)
         // setRemoteStream(event.stream);
       };
-      // const dc = lc.createDataChannel('channel');
-  
-      // dc.onmessage = (e) => {
-        
-      //   console.log('msg from B' + e.data);
-      //   setChats((prevChats) => {
-      //     console.log('prvchats',prevChats)
-      //     return [...prevChats, {"content":e.data}]
-      //   });
-      //   // setChats([...chats,e.data])
-      // }
-      // dc.onopen = (e) => {
 
-      //   console.log("connection opened!")
-      //   setConnectionOpened(true)
-
-      // };
-  
       lc.onicecandidate = async (e) => {
-        // if (e.candidate) {
-        //   // Candidate is available, call addRTCUserInfo
-        //   console.log("with_email let's fetch user",with_email)
-        //   const to_user_id = await fetchUserId(token,with_email)
-        //   console.log("whatisthisto_user_id",to_user_id)
-        //   addRTCUserInfo(true, JSON.stringify(lc.localDescription),to_user_id);
-        //   // console.log("Notice how many times it's being called...", JSON.stringify(lc.localDescription));
-        // }
-
         if (e.candidate) {
           // Candidate is available, but don't save it yet
           console.log("ICE candidate available");
@@ -254,9 +227,23 @@ export default function VideoWindow({ with_email,with_userid }) {
       // console.log("here is your offer love",offer,typeof(offer))
       const rc = new RTCPeerConnection()
       rc.onaddstream = (event) => {
+        console.log('AREWEHERE')
+
         myRef.current.channel.srcObject = event.stream;
         // setRemoteStream(event.stream);
         setStream(event.stream)
+      };
+
+      rc.ondatachannel = (event) => {
+        // Handle the data channel when it is created
+        const dataChannel = event.channel;
+  
+        dataChannel.onopen = () => {
+          console.log("Data channel opened!");
+          // You can add any specific actions you want to perform when the data channel is open.
+        };
+  
+        // Handle other data channel events if needed
       };
 
       rc.onicecandidate = async (e) => {
@@ -270,31 +257,11 @@ export default function VideoWindow({ with_email,with_userid }) {
         
       }
 
-      // rc.ondatachannel=e=>{
-
-      //     // rc=e.channel;
-
-      //     // rc.onmessage = e => {
-      //     //   // setChats(prv_chat=>[...prv_chat,e.data])
-      //     //   if (e.data){
-      //     //     // const old_chats = JSON.parse(JSON.stringify(chats))
-      //     //     // console.log("old_chats",old_chats)
-      //     //     // setChats([...old_chats,e.data])
-      //     //     setChats((prevChats) => {
-      //     //       console.log('prvchats',prevChats)
-      //     //       return [...prevChats, {"content":e.data}]
-      //     //     });
-
-      //     //     console.log("new message from client A!!",e.data)}
-      //     //   }
-
-      //     // rc.onopen = e => {
-      //     //   setConnectionOpened(true)
-      //     //   console.log("connection opened!")}
+      
 
           
 
-      // }
+
 
       rc.setRemoteDescription(offer).then(a=>{
         console.log("set remoteDescription with local offer")
@@ -414,6 +381,8 @@ export default function VideoWindow({ with_email,with_userid }) {
             "channel":lc,
             // "lc":lc
           }
+
+          
           // myRef.current = 'updated Value';
           
         }
@@ -434,73 +403,80 @@ export default function VideoWindow({ with_email,with_userid }) {
         }
 
       }
+      myRef.current.channel.addEventListener('iceconnectionstatechange', () => {
+            console.log('ICE Connection State changed:', myRef.current.channel.iceConnectionState);
+            
+            // if (myRef.current.channel.iceConnectionState === 'connected') {
+            //   // ICE connection is fully established
+            //   setConnectionOpened(true);
+            // }
+          });
+      
+      myRef.current.channel.addEventListener('connectionstatechange', () => {
+        console.log('Connection State changed:', myRef.current.channel.connectionState);
+      
+        // if (myRef.current.channel.connectionState === 'connected') {
+        //   // Connection is fully established
+        //   setConnectionOpened(true);
+        // }
+      });
+
+      myRef.current.channel.addEventListener('signalingstatechange', () => {
+        console.log('AREWHEREWREVER')
+        console.log(typeof(myRef.current.channel),myRef.current.channel,'Signaling State changed:', myRef.current.channel.signalingState, myRef.current.channel.iceConnectionState, myRef.current.channel.connectionState);
+        if (myRef.current.channel.signalingState === 'stable') {
+          console.log('current signalingState',myRef.current.channel.signalingState)
+          // setConnectionOpened(true);
+        }
+      });
+
       console.log("myRef's updated value:", myRef.current);
 
     }
   }, [with_email]);
 
 
-  const sendMsg=(msg)=>{
-    console.log("here is the msg",msg,myRef.current['type'])
-    setChats((prevChats) => {
-      console.log('prvchats',prevChats)
-      return [...prevChats, {"content":msg,"who":"ME"}]
-    });
-    if (myRef.current['type']=="INITIATOR"){
 
-
-    myRef.current.channel.send(msg)
-
-    }
-    else{
-      myRef.current.channel.send(msg)
-      
-    }
-
-    // myRef.current['type']=="INITIATOR"
-  }
   useEffect(() => {
     if(answer){
       console.log("HEREISWHATWEHAVE")
       console.log("if INITIATOR I THING WE CAN INITIATE THE CONNECTION??,",myRef.current,myRef.current['type']=="INITIATOR")
       if (myRef.current['type']=="INITIATOR"){
-      // let's perform the thrid step
-      console.log("hereAnswer",myRef.current,myRef.current.answer,typeof(myRef.current.answer))//myRef.current.send("douseeme!")
-      const answer = JSON.parse(myRef.current.answer)
-      console.log("DOWESEE ANSWER",answer)
-      // myRef.current.channel.send("douseeme!")
-      // answer = answer
-      console.log('Signaling State before setting remote description:', myRef.current.channel.signalingState);
+        // let's perform the thrid step
+        console.log("hereAnswer",myRef.current,myRef.current.answer,typeof(myRef.current.answer))//myRef.current.send("douseeme!")
+        const answer = JSON.parse(myRef.current.answer)
+        console.log("DOWESEE ANSWER",answer)
+        // myRef.current.channel.send("douseeme!")
+        // answer = answer
+        console.log('Signaling State before setting remote description:', myRef.current.channel.signalingState);
 
-      myRef.current.channel.setRemoteDescription(answer).then((a)=>{
         
-        console.log('dowseseethantythere')
-        console.log('Signaling State after setting answer on setRemoteDescription:', myRef.current.channel.signalingState);
 
-      }).then(a=>console.log('dowe also see this??',myRef.current.channel.signalingState))
-      console.log("didweneverreachhere??")
-
-        setConnectionOpened(true)
-      
-      // let i=0
-      // const intervalId = setInterval(() => {
-      //   i+=1
-      //   console.log('douseeme!!!',i)
-      //   myRef.current.channel.send(`msg from A-${i}`)
-
-      // }, 10000);
+        myRef.current.channel.setRemoteDescription(answer).then((a)=>{
+          
+          console.log('dowseseethantythere')
+          console.log('Signaling State after setting answer on setRemoteDescription:', myRef.current.channel.signalingState);
+          
+        })
+        .catch((error) => {
+          console.error('Error setting remote description:', error);
+        });
+     
       }
+      
 
     }
   }, [answer])
 
+ 
   console.log("answerishere",answer)
   useEffect(() => {
-    if(connection_open){
+    if(myRef.current.channel.connectionState=='connected'){
       console.log("connectionopened",connection_open)
+      setConnectionOpened(true)
 
     }
-  }, [connection_open])
+  }, [myRef.current.channel.connectionState])
   
 
   console.log("hereis",myRef.current)
@@ -508,7 +484,7 @@ export default function VideoWindow({ with_email,with_userid }) {
 
     <div>
 
-      {JSON.stringify(connection_open)}
+      {connection_open}
       <video
         ref={yourVideoRef} // Add a ref to the video element
         autoPlay
