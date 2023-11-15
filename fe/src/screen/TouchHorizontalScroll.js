8a18a7b3617cf370b72a58f9e9c0de04dd644ccb
deepak singh <codekeep18feb@gmail.com>
2023-11-15 14:29:35 +0530

    added horizontal scrollbar without scrollbar visible workes fine but not great

--- fe/src/screen/TouchHorizontalScroll.js
+++ fe/src/screen/TouchHorizontalScroll.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-// import { useSpring, animated, useGesture } from 'react-spring';
 import { useSpring, animated } from 'react-spring';
-import { useGesture } from 'react-use-gesture';
 
 const useStyles = makeStyles({
   scrollContainer: {
     overflow: 'hidden',
     width: '100%',
-    touchAction: 'pan-y',
-
   },
   innerContainer: {
     display: 'flex',
@@ -24,31 +20,33 @@ const useStyles = makeStyles({
   },
 });
 
-export default function InboxPractice() {
+export default function TouchHorizontalScroll() {
   const classes = useStyles();
 
   const [index, setIndex] = React.useState(0);
 
-  const bind = useGesture({
-    onDrag: ({ offset: [x], event: e, cancel }) => {
-      e.preventDefault();
-       // Adjust the sensitivity as needed
-       const sensitivity = 0.5;
-       const newIndex = Math.round(x / sensitivity / 310);
-       setIndex(newIndex);
-      // ... your existing logic
-    },
-  });
+  const handleWheel = (e) => {
+    // Adjust the sensitivity as needed
+    const sensitivity = 0.1;
+    const delta = e.deltaY || e.detail || e.wheelDelta;
+
+    // Update the index based on the wheel movement
+    setIndex((prevIndex) => prevIndex + Math.sign(delta) * sensitivity);
+  };
 
   const props = useSpring({
     transform: `translate3d(${-index * 310}px, 0, 0)`, // Adjust the width of the scroll item + margin
   });
 
   return (
-    <div className={classes.scrollContainer} {...bind()}>
+    <div className={classes.scrollContainer} onWheel={handleWheel}>
       <animated.div className={classes.innerContainer} style={props}>
-        <div className={classes.scrollItem}>Content 1</div>
-        <div className={classes.scrollItem}>Content 2</div>
+        <div className={classes.scrollItem}>
+          Content 1
+        </div>
+        <div className={classes.scrollItem}>
+          Content 2
+        </div>
         {/* Add more divs as needed */}
       </animated.div>
     </div>
