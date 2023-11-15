d981351ef82ea52351087c247e9e1b7e67b43d4f
deepak singh <codekeep18feb@gmail.com>
2023-11-15 14:41:33 +0530

    added horizontal scrollbar without scrollbar visible workes fine

--- fe/src/screen/HorizontalScrollWithinItemWidthOnly.js
+++ fe/src/screen/HorizontalScrollWithinItemWidthOnly.js
@@ -22,7 +22,7 @@ const useStyles = makeStyles({
   },
 });
 
-export default function InboxPractice() {
+export default function HorizontalScrollWithinItemWidthOnly() {
   const classes = useStyles();
   const innerContainerRef = useRef(null);
 
