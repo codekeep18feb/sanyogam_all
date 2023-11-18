e10cebeea1881c226324e67c7c9397ba0207690a
deepak singh <djangoboy.18feb@gmail.com>
2023-11-18 21:01:12 +0530

    imporoved UI EDIT PROFILE FLOW

--- fe/src/component/PreviewProfile.js
+++ fe/src/component/PreviewProfile.js
@@ -1,16 +1,93 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import { Grid, Typography } from "@mui/material";
+import React from "react";
+import Location from "@mui/icons-material/LocationOnOutlined";
+import People from "@mui/icons-material/PeopleAltOutlined";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import EditIcon from "@mui/icons-material/Edit";
 
+export const FamilyPreview = () => {
+  return (
+    <Grid container flexDirection={"column"}>
+      <Grid
+        container
+        justifyContent="space-between"
+        sx={{ padding: "5px 10px 0 10px" }}
+      >
+        <Grid item>
+          <div>Family Details</div>
+        </Grid>
+
+        <div style={{ display: "flex" }}>
+          <div style={{ opacity: 0.8 }}>
+            <a href="edit_family" style={{ textDecoration: "none" }}>
+              <Typography variant="subtitle2">Edit</Typography>
+            </a>
+          </div>
+
+          <EditIcon
+            style={{ fontSize: "17px", color: "blue", marginLeft: "5px" }}
+          />
+        </div>
+
+
+        
+      </Grid>
+
+      <div style={{ display: "flex",padding:"10px 0" }}>
+        <People
+          style={{ fontSize: "24px", color: "magenta", padding: "20px" }}
+        />
+
+        <div>
+          <Typography variant="subtitle2" sx={{ opacity: 0.7, color: "grey" }}>
+            Family Members{" "}
+          </Typography>
+          <Typography variant="subtitle2">
+            Father Mother 1 brother who is unmarried 1 sister who is married{" "}
+          </Typography>
+        </div>
+      </div>
+
+
+      <div style={{ display: "flex",padding:"10px 0" }}>
+        <Location
+          style={{ fontSize: "24px", color: "magenta", padding: "20px" }}
+        />
+
+        <div>
+          <Typography variant="subtitle2" sx={{ opacity: 0.7, color: "grey" }}>
+            Stay in
+          </Typography>
+          <Typography variant="subtitle2">
+            Noida
+          </Typography>
+        </div>
+      </div>
+
+
+      
+
+      
+    </Grid>
+  );
+};
+
+export const FatherDetails = () => {
+  return <div>FatherDetals</div>;
+};
 export default function PreviewProfile() {
   return (
     <>
-    <div>PreviewProfile</div>
-    <div style={{ marginTop: "10px", opacity: 0.8 }}>
-              <a href="edit_family" style={{ textDecoration: "none" }}>
-                <Typography variant="subtitle2">Edit Family</Typography>
-              </a>
-            </div>
+      <div
+        style={{
+          margin: "10px",
+          padding: "15px 0",
+          boxShadow: "0px 1px 10px rgba(0, 0, 0, 0.3)",
+        }}
+      >
+        <FamilyPreview />
+      </div>
+      <FatherDetails />
     </>
-    
-  )
+  );
 }
