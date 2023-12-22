0fbc4fe49bb5f4582953e089315678fa79b8691a
deepak singh <codekeep18feb@gmail.com>
2023-12-22 22:46:49 +0530

    added websocket as well :)

--- sanygam_be_v1/app.py
+++ sanygam_be_v1/app.py
@@ -1,19 +1,34 @@
 from flask import render_template,abort,jsonify # Remove: import Flask
-import config
+# import config
 from flask_migrate import Migrate
 # from handlers.users import read_all
+from flask_socketio import SocketIO, emit  # Import SocketIO
 
-from config import app, db  # Assuming your Flask app instance is named 'app'
+from config import app, db, socketio, connex_app,basedir  # Assuming your Flask app instance is named 'app'
 migrate = Migrate(app, db)
+# socketio = SocketIO(app)  # Initialize SocketIO
 
+# # ... existing code ...
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+# @socketio.on('message')
+# def handle_message(message):
+#     print('Received message:', message)
+#     socketio.emit('message', message)  # Echo the message b
 
+@socketio.on('message')
+def handle_message(message):
+    print('Received message:', message)
+    emit('message', message)  # Echo the message back to all clients
 
+# ...
 
-
-connex_app = config.connex_app
-connex_app.add_api(config.basedir / "swagger.yml")
+# connex_app = connex_app
+connex_app.add_api(basedir / "swagger.yml")
 
 
 if __name__ == "__main__":
-    connex_app.run(host="0.0.0.0", port=8000, debug=True)
+    # connex_app.run(host="0.0.0.0", port=8000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=8000, debug=True)
--- sanygam_be_v1/config.py
+++ sanygam_be_v1/config.py
@@ -5,10 +5,13 @@ import time
 import connexion
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager, jwt_required
+from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+from flask_socketio import SocketIO
+
 from flask.json import JSONEncoder
 from enum import Enum
+
 
 class CustomJSONEncoder(JSONEncoder):
     def default(self, obj):
@@ -54,33 +57,21 @@ def _current_timestamp() -> int:
 
 
 basedir = pathlib.Path(__file__).parent.resolve()
-print("AMICORRECT ? basedir",basedir)
+print("AMICORRECT ? basedir", basedir)
 connex_app = connexion.App(__name__, specification_dir=basedir)
-# Initialize CORS to allow requests from http://192.168.1.13:3000
-# CORS(connex_app.app)
-CORS(connex_app.app, resources={r"/api/*": {"origins": "http://192.168.1.13:3000"}})
+# CORS(connex_app.app, resources={r"/api/*": {"origins": "http://192.168.1.13:3000"}})
+CORS(connex_app.app, resources={r"/api/*": {"origins": ["http://192.168.1.13:3000", "http://localhost:8000"]}})
 
+# Attach SocketIO to the Flask app
+socketio = SocketIO(cors_allowed_origins="*")
+socketio.init_app(connex_app.app)
 
-
-
-@connex_app.app.after_request
-def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-    return response
-
-
+# ... (other configurations)
 
 app = connex_app.app
 app.json_encoder = CustomJSONEncoder
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'sgam.db'}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-# Add JWT configuration
-# app.config["JWT_SECRET_KEY"] = "your_secret_key"  # Replace with your secret key
-# jwt = JWTManager(app)
-
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-
+# jwt = JWTManager(app)
